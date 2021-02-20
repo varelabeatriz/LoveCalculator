2ae7528200986f37e653618d304e642aae03a870
Beatriz Varela <beatrizsv19@gmail.com>
2021-02-19 21:15:23 -0300

    images and styles

--- src/pages/Calculator/Calculator.jsx
+++ src/pages/Calculator/Calculator.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Title, Subtitle, Button, Input } from './styles';
+import { Title, Subtitle, Button, Input, Wrapper, Hearts, ButtonWrapper } from './styles';
+import hearts from './img/hearts.png'
 
 const Calculator = () => {
     const [name, setName] = useState('')
@@ -8,29 +9,29 @@ const Calculator = () => {
     let result = name.length * secondName.length;
     let loveResult = 0;
 
-    if (result % 2 == 0){
+    if (result % 2 == 0) {
         loveResult = result * 3
     } else {
         loveResult = result * 4
     }
 
-    if (loveResult > 100){
+    if (loveResult > 100) {
         loveResult = 100;
     }
 
-    if (loveResult < 0){
+    if (loveResult < 0) {
         loveResult = 0;
     }
 
     if ((name == "Beatriz") || (secondName == "Beatriz")) {
-        if((name == "Gabriel") || (secondName == "Gabriel")){
+        if ((name == "Gabriel") || (secondName == "Gabriel")) {
             loveResult = 100;
         }
     }
 
     return (
-        <>
-            <Title>Love calculator</Title>
+        <Wrapper>
+            <Title>Love Calculator</Title>
             <Subtitle>Be sure that you are right for each other</Subtitle>
             <div>
                 <Input
@@ -38,18 +39,24 @@ const Calculator = () => {
                     onChange={event => setName(event.target.value)}
                 >
                 </Input>
+                <Hearts
+                    alt="hearts"
+                    src={hearts}
+                    style={{ width: 100, height: 100 }}
+                />
                 <Input
                     placeholder="Your lover's name"
                     onChange={event => setSecondName(event.target.value)}
                 >
                 </Input>
             </div>
-            <Button
-                onClick={console.log(name)}
-            >Calculate</Button>
+            <ButtonWrapper>
+                <Button
+                    onClick={console.log(name)}
+                >Calculate</Button>
+            </ButtonWrapper>
             <p>{loveResult}%</p>
-
-        </>
+        </Wrapper>
     )
 }
 
--- src/pages/Calculator/styles.js
+++ src/pages/Calculator/styles.js
@@ -1,10 +1,16 @@
+import { Redirect } from 'react-router-dom';
 import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    background-color: #FFFCF9;    
+`;
 
 export const Title = styled.p`
     @import url('https://fonts.googleapis.com/css2?family=Vidaloka&display=swap');
     color: #444;
     font-family: 'Vidaloka', serif;
     font-size: 72px;
+    margin: 10% 0 -10px 0;
 `;
 
 export const Subtitle = styled.p`
@@ -25,9 +31,34 @@ export const Button = styled.button`
     width: 230px;
     height: 55px;
     border: 0px;
+    margin-top: 16px;
+    transition: 400ms;
+    outline: none;
     `;
 
-    export const Input = styled.input`
+    export const ButtonWrapper = styled.div`
+    &:hover ${Button} {
+        background-color: #EC58C5;
+    }
+    `;
+
+export const Input = styled.input`
         height: 40px;
         margin: 14px;
+        border-radius: 16px;
+        border: 0px;
+        box-shadow: 0px 4px 8px 1px rgba(0, 0, 0, 0.25);
+        outline: none;
+        padding-left: 10px;
+        
     `;
+
+export const Hearts = styled.img`
+   max-width: 600px;
+   max-height: 600px;
+   position: relative;
+
+`;
+
+
+
